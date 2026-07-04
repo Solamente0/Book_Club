5acbe3ccc4d429162319b0b0ee988c862e1fcc4a
niki <nikitaadelnia@gmail.com>
2026-07-04 05:12:55 +0400

    feat: add class publisher

---
+++ Common/publisher.cpp
@@ -0,0 +1,39 @@
+#include "Publisher.h"
+
+Publisher::Publisher()
+    : Member(), totalRevenue(0.0) {};
+
+Publisher::Publisher(const QString &username, const QString &password, const QString& securityAnswer)
+    : Member(username, password, securityAnswer),
+      totalRevenue(0.0) {};
+
+QString Publisher::role() const{return "Publisher";}
+
+QVector<Book> Publisher::getPublishedBooks() const{ return publishedBooks;}
+
+void Publisher::addPublishedBook(const Book &book){
+    publishedBooks.append(book);
+}
+
+void Publisher::removePublishedBook(int bookId) {
+    for (int i = 0; i < publishedBooks.size(); ++i) {
+        if (publishedBooks[i].getId() == bookId) {
+            publishedBooks.removeAt(i);
+            break;
+        }
+    }
+}
+
+void Publisher::updatePublishedBook(const Book &updated) {
+    for (Book &b : publishedBooks) {
+        if (b.getId() == updated.getId()) {
+            b = updated;
+            break;
+        }
+    }
+}
+
+double Publisher::getTotalRevenue() const{ return totalRevenue;}
+void Publisher::setTotalRevenue(double revenue) { totalRevenue = revenue;}
+
+int Publisher::numberOfPublishedBook() {return publishedBooks.size();}
