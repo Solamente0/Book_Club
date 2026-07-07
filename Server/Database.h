6a59b0b7b64b1ab431dfe20353823dd64798e252
niki <nikitaadelnia@gmail.com>
2026-07-07 04:25:47 +0400

    feat: add database class

---
+++ Server/Database.h
@@ -0,0 +1,40 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QString>
+#include <QDebug>
+
+class Database
+{
+public:
+    static Database& instance();
+
+    bool connect(const QString& dbPath = "data/bookclub.db");
+    bool isConnected() const;
+    void close();
+
+    QSqlQuery createQuery() const;
+
+private:
+    Database() = default;
+    Database(const Database& ) = delete;
+    Database& operator=(const Database& ) = delete;
+
+    bool createTables();
+    bool createMembersTable();
+    bool createBooksTable();
+    bool createPurchasesTable();
+    bool createCartsTable();
+    bool createReviewsTable();
+    bool createSavedBooksTable();
+    bool createShelvesTable();
+    bool createShelfBooksTable();
+    bool createNotificationsTable();
+
+    QSqlDatabase db;
+};
+
+#endif
