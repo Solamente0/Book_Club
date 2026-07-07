8cc130bd221eb43aa8a937b6719601043b942c45
niki <nikitaadelnia@gmail.com>
2026-07-07 04:15:43 +0400

    feat: implement book purchase

--- Common/users/User.h
+++ Common/users/User.h
@@ -2,22 +2,22 @@
 #define USER_H
 
 #include <QVector>
-#include <QMap>
 
 #include "../models/Book.h"
 #include "Member.h"
 #include "../models/Cart.h"
 #include "../models/Shelf.h"
+#include "../models/Purchase.h"
 
 class User: public Member
 {
 private:
     QVector<genre> favoriteGenres;
-    QVector<Book> purchasedBooks;
+    QVector<Purchase> purchasedBooks;
     QVector<Book> savedBooks;
     Cart cart;
-    QMap<int, int> lastReadPages;
     QVector<Shelf> shelves;
+    int purchasesCount;
     bool firstLogin;
 public:
     User();
@@ -27,21 +27,26 @@ public:
     QVector<genre> getfavoriteGenres() const;
     void setFavoriteGenres(const QVector<genre> &genres);
     void addFavoriteGenre(const genre &Genre);
-    QVector<Book> getpurchasedBooks() const;
-    void addPurchasedBook(const Book &book);
+
+    QVector<Purchase> getpurchasedBooks() const;
+    void addPurchasedBook(const Purchase &book);
     bool hasPurchasedBook(int bookId) const;
+
     QVector<Book> getSavedBooks() const;
     void addSavedBook(const Book &book);
     void removeSavedBook(int bookId);
     bool hasSavedBook(int bookId) const;
+
     Cart& getCart();
-    int getlastReadPage(int bookId) const;
-    void setLastReadPage(int bookId, int page);
+
     QVector<Shelf> getShelves() const;
     void addShelf(const Shelf &shelf);
     void removeShelf(const QString &shelfName);
     void renameShelf(const QString &oldName, const QString &newName);
     Shelf* findShelf(const QString &name); 
+
+    int lastReadPage(int bookId) const;
+    void setLastReadPage(int bookId, int page);
 
     bool isFirstLogin() const;
     void setFirstLogin(bool first);
